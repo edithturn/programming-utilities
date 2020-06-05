977698ee7156ed4fbe8427d713f79ac9f728779a
edithturn <edithpuclla20@gmail.com>
2020-06-05 12:17:30 -0700

    removing files

--- C-Programming/hash-table/hash-table.c
+++ C-Programming/hash-table/hash-table.c
@@ -12,14 +12,41 @@ typedef struct {
 	int age;
 }	person;
 
+person * hash_table[TABLE_SIZE];
+
 unsigned int hash(char *name){
-	return 5;
+	int lenght = strnlen(name, MAX_NAME);
+	unsigned int hash_value = 0;
+	for (int i=0; i < lenght; i++){
+		hash_value  += (name[i] % TABLE_SIZE);
+	}
+	return hash_value;
+}
+
+void print_table(){
+	for (int i=0; i < TABLE_SIZE; i++){
+		if (hash_table[i] == NULL){
+			printf("\t%i\t---");
+		} else {
+			printf("\t%i\t%s\n", hash_table[i]->name);
+		}
+	}
+}
+void init_hash_table(){
+	for (int i=0; i < TABLE_SIZE; i++){
+		hash_table[i] = NULL;
+	}
 }
 
 int main(){
-	printf("Edith => %u\n", hash("Edith"));
+	init_hash_table();
+	printf("Edith => %u\n", hash("Edi"));
 	printf("Pavel => %u\n", hash("Pavel"));
 	printf("Marcia => %u\n", hash("Marcia"));
 	printf("Felix => %u\n", hash("Felix"));
 	printf("Jean => %u\n", hash("Jean"));
+	printf("Tomas => %u\n", hash("Tomas"));
+	printf("Jane => %u\n", hash("Jane"));
+	printf("Terence => %u\n", hash("Terence"));
+	printf("Elián => %u\n", hash("Elián"));
 }
